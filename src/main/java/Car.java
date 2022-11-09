9daa2a4edfcc91903b3e9fd264e873ea987b4673
ankit <ankit.kishorelv@gmail.com>
2022-11-09 20:17:33 +0530

    called method in main methods from different classes

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -5,5 +5,30 @@
  */
 
 
-public class Car {
+public class Car extends VehicleManufacturer implements Vehicle {
+    public Car(String vehicleName, String vehicleModelName, String vehicleType) {
+        super(vehicleName, vehicleModelName, vehicleType);
+
+    }
+
+    public String getManufacturerInformation() {
+        return "Bike{" +
+                "Manufacturer Name='" + getVehicleName() + '\'' +
+                ", Model Name =" + getVehicleModelName() +
+                ", Type = " + getVehicleType() +
+                '}';
+    }
+    public int maxSpeed(String vehicleType)
+    {
+        if(vehicleType.equals("SportsCar"))
+        {
+            return 250;
+        }
+        if (vehicleType.equals("Sedan"))
+        {
+            return 170;
+        }
+        return 0;
+
+    }
 }
